0e43abb9d170fffa0e5803ab6a6aae539ffcf23e
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-22 14:44:30 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="">
       <Header />
       <Card />
     </div>
