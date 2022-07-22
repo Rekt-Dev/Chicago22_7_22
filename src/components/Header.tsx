4abc5838b777f613976e75837ebd54ff8b399127
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-07-22 14:42:06 +0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ export let Header = (props: any) => {
         <h1>The Art Institute of Chicago</h1>
         <h2>Search through the museums DB !</h2>
         <input>search</input>
+        <button>search</button>
       </div>
     </>
   );
