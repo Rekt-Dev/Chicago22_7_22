2bce41527e56912c26a967a6a8ab8cb0acc75cf8
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-01 00:25:11 +0300

    added react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -8,6 +8,8 @@ const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <Router />
     <App />
+    <Router />
   </StrictMode>
 );
