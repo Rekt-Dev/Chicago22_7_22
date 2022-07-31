a7c677dd5374ea350dcc0e5eb0974b2e03189c07
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-01 01:00:05 +0300

    .

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -1,3 +1,7 @@
 export const OpenCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <img src="https://www.123rf.com/photo_41026708_example-white-stamp-text-on-red-backgroud.html" />
+    </div>
+  );
 };
