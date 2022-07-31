54c4fa75c513b8275d7aadfb749a892871593218
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-01 01:06:16 +0300

    .

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 export const OpenCard = () => {
   return (
     <div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
 import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 import App from "./App";
+import { OpenCard } from "./components/OpenCard";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Router />
-    <App />
-    <Router />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<OpenCard />} />
+          <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   </StrictMode>
 );
