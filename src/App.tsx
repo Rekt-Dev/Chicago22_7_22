135f22de42f3727518283dce21a55f4fde32726c
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-04 10:49:41 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,17 @@ import "./styles.css";
 import { Card } from "./components/Card";
 import { Header } from "./components/Header";
 import { useState } from "react";
-import {
-  BrowserRouter as 
-  Route,
-  Link,
-  useNavigate
-} from "react-router-dom";
 
 export default function App() {
   const [jsonData, setJsonData] = useState([]);
 
   async function getData(searchData: any) {
-    let data1;
     console.log("getdATA invoked");
     const response = await fetch(
       `https://api.artic.edu/api/v1/artworks/search?q=${searchData}&limit=10&fields=id,title,image_id,artist_names,artist_title,place_of_origin`
     );
     setJsonData(await response.json());
     console.log(jsonData);
-    data1 = jsonData;
   }
 
   return (
