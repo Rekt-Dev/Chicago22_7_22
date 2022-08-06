888c20c0c7713af86063b2dbfb4390baa5f18439
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-06 21:09:10 +0300

    .

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useNavigate} from "react-router-dom";
 
 export const OpenCard = () => {
   return (
