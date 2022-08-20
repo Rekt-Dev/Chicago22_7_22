d688a51152eb1447368fc9c6153c6aa5af48a6f2
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-20 17:15:12 +0300

    .

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,32 @@
 //import { Redirect } from "./Redirect";
 import Image from "react-bootstrap/Image";
-import { OpenCard } from "./OpenCard";
 export let Card = (props: any) => {
+  const OpenCard = (props: any) => {
+    function Default() {
+      const defaultLink =
+        "https://www.artic.edu/iiif/2/d68a3f34-411d-efd4-154e-48b9c565b125/full/843,/0/default.jpg";
+
+      if (props.item) {
+        return props.item.image_id;
+      } else return defaultLink;
+    }
+    //let navigate = useNavigate();
+
+    /* const clickCard = () => {
+      console.log(`clickCard activ8d`);
+    };
+   */
+
+    return (
+      <div>
+        <img
+          alt="beauty"
+          src={`https://www.artic.edu/iiif/2/${props.item.image_id}/full/843,/0/default.jpg`}
+        />
+      </div>
+    );
+  };
+
   function truncate(str) {
     if (str) {
       return str.length > 10 ? str.substring(0, 7) + "..." : str;
