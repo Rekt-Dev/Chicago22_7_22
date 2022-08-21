9f6158c8af18117189e1211ea4e78befb29a7734
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-21 17:50:36 +0300

    .

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -2,10 +2,16 @@ import { Form } from "react-bootstrap";
 /* import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom"; */
 
-export function OpenCard() {
+export function OpenCard(props: any) {
   return (
     <div>
       <h1>Open Card page</h1>
+      <div>
+        <img alt="love">
+          src=
+          {`https://www.artic.edu/iiif/2/${props.item.image_id}/full/843,/0/default.jpg`}
+        </img>
+      </div>
     </div>
   );
 }
