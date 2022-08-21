063f43d72d8d4141c7239a603629ae300b1d0cbf
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-08-21 07:07:30 +0300

    deleting a hrefs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,16 @@
 //import { Redirect } from "./Redirect";
 import Image from "react-bootstrap/Image";
 export let Card = (props: any) => {
+  const OpenCard = (props: any) => {
+    return (
+      <div>
+        <img
+          alt="beauty"
+          src={`https://www.artic.edu/iiif/2/${props.item.image_id}/full/843,/0/default.jpg`}
+        />
+      </div>
+    );
+  };
   function truncate(str) {
     if (str) {
       return str.length > 10 ? str.substring(0, 7) + "..." : str;
@@ -20,63 +30,45 @@ export let Card = (props: any) => {
 
   //const artworkId = props.item.image_id;
   //let constructedLink = imageStartLink + artworkId + imageEndLink;
-  let goToArtist = () => {
-    const OpenCard = (props: any) => {
-      return (
-        <div>
-          <img
-            alt="beauty"
-            src={`https://www.artic.edu/iiif/2/${props.item.image_id}/full/843,/0/default.jpg`}
-          />
-        </div>
-      );
-    };
-  };
+  let goToArtist = () => {};
   return (
     <div onClick={OpenCard} className="">
       <div className={("", "")}>
         <div className={("", "", "", "card")}>
-          <a href="./OpenCard">
-            <Image
-              onError={({ currentTarget }) => {
-                currentTarget.onerror = null; // prevents looping
-                currentTarget.src =
-                  "http://books.google.com/books/content?id=FDJBgvXyZrsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
-              }}
-              roundedCircle
-              alt=" 3 cats"
-              height="300"
-              width="300"
-              src={`https://www.artic.edu/iiif/2/${props.item.image_id}/full/843,/0/default.jpg`}
-              onMouseOut={() => console.log("out of hover")}
-              onMouseOver={() => console.log("into hover")}
-            />
-            <div
-              className=""
-              style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
-            ></div>
-          </a>
+          <Image
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null; // prevents looping
+              currentTarget.src =
+                "http://books.google.com/books/content?id=FDJBgvXyZrsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
+            }}
+            roundedCircle
+            alt=" 3 cats"
+            height="300"
+            width="300"
+            src={`https://www.artic.edu/iiif/2/${props.item.image_id}/full/843,/0/default.jpg`}
+            onMouseOut={() => console.log("out of hover")}
+            onMouseOver={() => console.log("into hover")}
+          />
+          <div
+            className=""
+            style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" }}
+          ></div>
           <ul>
             {}
-            <a href="./GoToArtist">
-              <p className={("truncate", "")} onClick={goToArtist}>
-                Title: {truncate(props.item.title) || "Artist name"}
-              </p>
-            </a>{" "}
-            <a href="./GoToArtist">
-              <p className={("truncate", "alignLeft")} onClick={goToArtist}>
-                Artist: {truncate(props.item.artist_title) || "Title Name"}
-              </p>
-            </a>
-            <a href="./GoToArtist">
-              <p
-                title={props.item.place_of_origin}
-                className={("truncate", "alignLeft")}
-                onClick={goToArtist}
-              >
-                Origin: {truncate(props.item.place_of_origin) || "origin"}
-              </p>
-            </a>
+            <p className={("truncate", "")} onClick={goToArtist}>
+              Title: {truncate(props.item.title) || "Artist name"}
+            </p>
+
+            <p className={("truncate", "alignLeft")} onClick={goToArtist}>
+              Artist: {truncate(props.item.artist_title) || "Title Name"}
+            </p>
+            <p
+              title={props.item.place_of_origin}
+              className={("truncate", "alignLeft")}
+              onClick={goToArtist}
+            >
+              Origin: {truncate(props.item.place_of_origin) || "origin"}
+            </p>
           </ul>
         </div>
       </div>
